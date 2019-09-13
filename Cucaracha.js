4a1c0e275705bb021f357fd359b1b07256370521
Lucas Simper <lucassimper@MacBook-Pro.local>
2019-09-13 13:53:46 +0200

    my branch lucas

---
+++ Cucaracha.js
@@ -0,0 +1,14 @@
+//This code doesnt do shit yet wuhuuuu
+
+//DFUkdsamkfmsadlfg
+
+fuck up some kommas
+
+//Jeg har lagt en lort her
+
+// Der er nu 2 lorte
+
+//Der er allerede kommet 3 lorte
+
+jeg vil lave en ædsafgd
+
